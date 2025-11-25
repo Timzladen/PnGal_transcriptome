eaeedfc06b7e6bffa646df28a044ac31ca8f9b0c
Timzladen <turk.tim@siol.net>
2025-11-25 13:01:37 +0100

    Update assembly_workflow.sh

--- assembly_workflow.sh
+++ assembly_workflow.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 #run the assembly on CLC Genomics QCed and trimmed fastq files
+conda activate spades
 bash assembly.sh
-
+conda deactivate
 #cluster contigs
+conda activate cd-hit
 bash cluster.sh
-
+conda deactivate
 #run salmon read quantification
+conda activate salmon
 bash salmon.sh
+conda deactivate
+
+#annotate contigs with eggnog
+emapper.py \
+  -i transcripts.fasta \
+  -o eggnog_annotations \
+  --cpu 64 \
+  -m diamond \
+  --itype CDS \
+  --data_dir eggnogDB
